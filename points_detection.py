560a5f09a6545e09fe5f52db70c81150fd5644d7
Mau Munguia <maumg1196@gmail.com>
2019-04-09 19:11:34 -0500

    Not Ready Point Detector

---
+++ points_detection.py
@@ -0,0 +1,41 @@
+"""Point Detector."""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+def rgb2gray(image):
+    """Transform a color image to a grayscale image."""
+    return np.dot(image[..., :3], [0.299, 0.587, 0.114])
+
+color = mpimg.imread('lobo.jpg')
+gray = rgb2gray(color)
+
+umbral = int(input('Ingrese un valor de umbral: '))
+umbral_matrix = np.array([[1, 1, 1,], [1, umbral, 1,], [1, 1, 1,]])
+
+y = gray.shape[0]
+x = gray.shape[1]
+
+newimage = np.zeros(gray.shape)
+for j in range(1, y - 1):
+    for i in range(1, x - 1):
+        new_value = np.sum(gray[j-1:j+2,i-1:i+2] * umbral_matrix)
+        if abs(new_value) > umbral:
+            newimage[j:i] = 255
+        else:
+            newimage[j:i] = 0
+
+plt.figure("Images")
+
+plt.subplot(1, 2, 1)
+plt.title("Gray")
+plt.imshow(gray, cmap=plt.get_cmap('gray'))
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.title("Points")
+plt.imshow(newimage, cmap=plt.get_cmap('gray'))
+plt.axis('off')
+
+plt.show()
